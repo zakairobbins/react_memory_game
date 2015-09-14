877d352a2c9ccb17cf8c0fdbf6fa8bf6593fbaa9
zakairobbins <zakairobbins@gmail.com>
2015-09-14 11:20:51 -0700

    create 6 card, 2 of each arry hack. Need to fix this later

--- app/assets/javascripts/pictures_ary.js.jsx
+++ app/assets/javascripts/pictures_ary.js.jsx
@@ -1,4 +1,4 @@
-var pictures = ["/assets/aquaman.jpg", "/assets/atom.jpg", "/assets/batman.png", "/assets/drfate.jpg", "/assets/greenlantern.jpg", "/assets/hawkman.jpg", "/assets/hourman", "/assets/justiceleague.jpg", "/assets/justicesociety.jpg", "/assets/martianmanhunter.jpg", "/assets/sandman.jpg", "/assets/spectre.jpg", "/assets/superman.jpg", "/assets/theflash.jpg", "/assets/wonderwoman.jpg"]
+var pictures = ["/assets/batman.png", "/assets/greenlantern.jpg", "/assets/justiceleague.jpg", "/assets/sandman.jpg", "/assets/superman.jpg", "/assets/wonderwoman.jpg", "/assets/batman.png", "/assets/greenlantern.jpg", "/assets/justiceleague.jpg", "/assets/sandman.jpg", "/assets/superman.jpg", "/assets/wonderwoman.jpg"]
 
 function randomNum(min, max){
   return Math.floor(Math.random() * (max) + min);
@@ -10,3 +10,6 @@ function assignUnique(pictures) {
     return pictures.splice(idx, 1);
   };
 }
+
+
+// var pictures = ["/assets/aquaman.jpg", "/assets/atom.jpg", "/assets/batman.png", "/assets/drfate.jpg", "/assets/greenlantern.jpg", "/assets/hawkman.jpg", "/assets/hourman", "/assets/justiceleague.jpg", "/assets/justicesociety.jpg", "/assets/martianmanhunter.jpg", "/assets/sandman.jpg", "/assets/spectre.jpg", "/assets/superman.jpg", "/assets/theflash.jpg", "/assets/wonderwoman.jpg"]
