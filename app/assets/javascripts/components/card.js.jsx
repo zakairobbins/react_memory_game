fde89e44c2abb6b43d76315cd5414e296d76361b
zakairobbins <zakairobbins@gmail.com>
2015-09-13 18:12:22 -0700

    create make-deck function, not working properly

--- app/assets/javascripts/components/card.js.jsx
+++ app/assets/javascripts/components/card.js.jsx
@@ -1,11 +1,10 @@
 var Card = React.createClass({
   getInitialState: function() {
     return {
-      pic: pictures[randomNum(0, 14)]
+      pic: assignUnique(pictures)
     };
   },
   render: function() {
-    console.log(this.state.pic);
     return(
       <img className="cardPic"src={this.state.pic} alt="comic" />
     );
--- app/assets/javascripts/components/game_board.js.jsx
+++ app/assets/javascripts/components/game_board.js.jsx
@@ -1,5 +1,14 @@
+  var makeDeck = function() {
+      var deck = []
+    for (var i = 0; i <= 6; i++) {
+      deck[i] = <Card />
+    };
+    return deck;
+  }
 var GameBoard = React.createClass({
   render: function() {
+    {var deck = makeDeck()}
+    console.log(deck);
     return(
       <div className="board">
         <div className="row">
